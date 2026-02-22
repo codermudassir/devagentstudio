f5449880478635b8f49a8147854ec497a4dab6a3
Muhammad Mudassir <muhammad.mudassir@goenterprise.io>
2026-02-22 20:51:54 +0500

    update cod

--- scripts/check-user.ts
+++ scripts/check-user.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import * as dotenv from "dotenv";
-dotenv.config();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
